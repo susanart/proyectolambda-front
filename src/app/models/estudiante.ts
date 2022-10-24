07abc086075372a1fa727a1811e8593c362d2623
susanart <susanaarjs@gmail.com>
2022-10-24 07:57:23 -0500

    listando estudiantes

--- src/app/models/estudiante.ts
+++ src/app/models/estudiante.ts
@@ -3,16 +3,18 @@ export class Estudiante {
 
     id: number;
     idMateria: number;
-    notas: number;
+    nombre: string;
+    notas: any[];
 
 
 
 
    
 
-    constructor(id: number, idMateria: number, notas: number) {
+    constructor(id: number, idMateria: number, nombre:string, notas: any[]) {
         this.id = id;
         this.idMateria = idMateria;
+        this.nombre = nombre;
         this.notas = notas;
     }
 
