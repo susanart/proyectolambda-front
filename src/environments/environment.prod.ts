0cbeec95bb1dc4cd3f558e3bd075834bbe5b1fc9
JohnnyQuintero16 <johnnyaquintero@gmail.com>
2022-10-24 22:11:37 -0500

    update enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  materiaURL: 'https://fxkl2w0smd.execute-api.us-east-1.amazonaws.com/api/materia/',
+  estudiantesURL:  'https://l9rqohmxg0.execute-api.us-east-1.amazonaws.com/api/estudiante'
 };
